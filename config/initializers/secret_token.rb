0004dcb8a6946c71b3adae8d6f0640174d57d4f9
Max McCarty <max.mccarty@verizon.net>
2013-03-18 18:04:07 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fatmodels::Application.config.secret_token = 'd2d2b1143e84e20aafb554be78fd1ac68e014b1d7470306a594fec4b771d7927b69d922266d396663876b095cbaf9221083f478dec114540017661b6d2dce323'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Fatmodels::Application.config.session_store :cookie_store, key: '_fatmodels_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Fatmodels::Application.config.session_store :active_record_store
