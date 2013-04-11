a9da00c9e65d0fcee9019602daaa2a5dcf0426b1
Max McCarty <max.mccarty@verizon.net>
2013-04-11 19:24:05 -0400

    added seller model

--- app/models/seller.rb
+++ app/models/seller.rb
@@ -1,11 +1,15 @@
 class Seller < DelegateClass(User)
     delegate :id, to: :__getobj__
-
     def has_seller_transactions?
         return false if self.orders.empty?
 
         self.orders.each do |order|
           return true unless order.has_seller_transaction.nil?
         end
-      end
+     end
+
+     def has_market_place?
+        self.has_market_places.empty?
+     end
+
 end
--- spec/models/seller_spec.rb
+++ spec/models/seller_spec.rb
@@ -4,10 +4,7 @@ describe Seller do
     before {@seller = Seller.new(FactoryGirl.create(:user))}
 
     subject {@seller}
+    it {should respond_to(:name, :market_places)}
 
-    it {should respond_to(:name, :market_place)}
 
-    describe "when has seller transactions" do
-        before { }
-    end
 end
